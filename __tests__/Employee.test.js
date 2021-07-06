f18abc98de79ff51a973265369d3189e26647717
JazmyneB <bradleyj1@email.arizona.edu>
2021-07-06 16:33:57 -0700

    test Employee Object

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,22 @@
+
+
+const Employee = require("../lib/Employee");
+
+//Employee Object test
+test('creates a Employee object', () => {
+    const employ = new Employee('Lee', '1234', 'leehub@gmail.com');
+
+    expect(employ.name).toBe('Lee');
+    expect(employ.id).toBe('1234');
+    expect(employ.email).toBe('leehub@gmail.com');
+});
+
+//Method --> GetName Test
+test('gets Employee Methods: Name, ID, EMail, Role', () =>{
+    const employ = new Employee('Lee', '1223', 'leehub@gmail.com');
+
+    expect(employ.getName()).toBe('Lee');
+    expect(employ.getId()).toBe('1223');
+    expect(employ.getEmail()).toBe('leehub@gmail.com');
+    expect(employ.getRole()).toEqual('Employee');
+})
