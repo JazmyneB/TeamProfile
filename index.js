f5b0280695bfac52d51656098e12676616ee6e56
JazmyneB <bradleyj1@email.arizona.edu>
2021-07-07 19:48:45 -0700

    added prompt questions

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 const Inquirer = require('inquirer')
 //Generates and starts application
 
-Inquirer
-    .prompt([
+
+const EmployeeInfo = () => {
+    return Inquirer.prompt([
         {
             type: 'input',
-            message: 'What  '
-        }
+            name: 'name',
+            message: 'What is the name?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the name of Employee!');
+                  return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'What is the Employee ID?',
+            validate: idInput => {
+                if (idInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the EMployee ID!');
+                  return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the email of Employee!');
+                  return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is the role of the Employee?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        // {
+        //     type: 'confirm',
+        //     name: 'confirmEmployee',
+        //     message: 'Would you like to add another Employee?',
+        //     default: false
+        // }
+
     ])
+}
+
+EmployeeInfo();
