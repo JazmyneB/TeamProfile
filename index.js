ea72a64f6dcad51618c77c29ec556f0ac7350a85
JazmyneB <bradleyj1@email.arizona.edu>
2021-07-09 00:16:47 -0700

    working on Manager accuracy of questions and user input

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const Inquirer = require('inquirer')
-//Generates and starts application
+const Inquirer = require('inquirer');
+const fs = require('fs');
+//Need Class files
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const { writeFile, copyFile } = require('./src/generate-site');
+const generatePage = require('./src/page-template');
+
+const team = [];
 
 
 const EmployeeInfo = () => {
-    return Inquirer.prompt([
+    Inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -49,14 +59,90 @@ const EmployeeInfo = () => {
             message: 'What is the role of the Employee?',
             choices: ['Manager', 'Engineer', 'Intern']
         },
-        // {
-        //     type: 'confirm',
-        //     name: 'confirmEmployee',
-        //     message: 'Would you like to add another Employee?',
-        //     default: false
-        // }
+        {
+            type: 'confirm',
+            name: 'confirmEmployee',
+            message: 'Would you like to add another Employee?',
+            default: false
+        }
 
-    ])
+    ]).then(answer => {
+        //console.log(answer);
+        if (answer.role === 'Manager'){
+            Inquirer.prompt([
+                {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the Office Number?'
+
+                }
+            ]).then(data =>{
+                //console.log(data);
+                //console.log(answer);
+                const newMange = new Manager(answer.name, answer.Id, answer.email, data.officeNumber);
+                //console.log(newMange);
+                team.push(newMange);
+                if (answer.confirmEmployee){
+                    return EmployeeInfo(team);
+                } else {
+                    return team;
+                }
+            })
+            
+        }
+        else if (answer.role === 'Engineer'){
+            Inquirer.prompt([
+                {
+                type: 'input',
+                name: 'gitHub',
+                message: 'What is their GitHub?'
+
+                }
+            ]).then(data => {
+                const newEngin = new Engineer(answer.name, answer.Id, answer.email, data.gitHub);
+                team.push(newEngin);
+            })
+        }
+        else if (answer.role === 'Intern'){
+            Inquirer.prompt([
+                {
+                type: 'input',
+                name: 'School',
+                message: 'What school do they attend?'
+                }
+            ]).then(data => {
+                const newIntern = new Intern(answer.name, answer.Id, answer.email, data.School);
+                team.push(newIntern);
+            })
+        }
+        // console.log(team)
+        // setTimeout(function() {
+        //     if (answer.confirmEmployee === true){
+        //         EmployeeInfo();
+        //     }
+
+        // },4000)
+
+    })
 }
 
+
+
 EmployeeInfo();
+
+// .then(info =>{
+//     return generatePage(info);
+// })
+// .then(pageHTML =>{
+//     return writeFile(pageHTML);
+// })
+// .then(writeFileResponse => {
+//     console.log(writeFileResponse);
+//     return copyFile();
+// })
+// .then(copyFileResponse => {
+//     console.log(copyFileResponse);
+// })
+// .catch(err =>{
+//     console.log(err);
+// });
